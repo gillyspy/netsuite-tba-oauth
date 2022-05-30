d6a5b0be78d4f5a00810d3b2b5563182ed4fa66f
geraldgillespie <gerald.gillespie@inscio.com>
2022-05-30 16:52:33 -0400

    fixed issue with method detection case-sensitivity

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
-'use strict';
 // Dependencies
 const request = require('request');
 const OAuth = require('oauth-1.0a');
 const crypto = require('crypto');
 
-module.exports = NetSuiteOAuth;
-
+/* eslint-disable camelcase */
 /**
  * @description Constructor
  * @param url
@@ -19,55 +17,56 @@ module.exports = NetSuiteOAuth;
  * @returns {PromiseLike<ArrayBuffer>}
  * @constructor
  */
-function NetSuiteOAuth(url, method, consumerKey, consumerSecret, tokenId, tokenSecret, account,signature_method='HMAC-SHA256') {
-    const algoToSigMethodMap = {
-        'HMAC-SHA256' : 'sha256'
-    };
+// eslint-disable-next-line camelcase
+function NetSuiteOAuth(url, method, consumerKey, consumerSecret, tokenId, tokenSecret, account, signature_method = 'HMAC-SHA256') {
+  const algoToSigMethodMap = {
+    'HMAC-SHA256': 'sha256'
+  };
 
-    this.request_data = {
-        url: url,
-        method: method
-    };
+  this.request_data = {
+    url,
+    method
+  };
 
-    this.token = {
-        key: tokenId,
-        secret: tokenSecret
-    };
+  this.token = {
+    key: tokenId,
+    secret: tokenSecret
+  };
 
-    let hash_function = (  base_string, key, algorithm = algoToSigMethodMap[signature_method] ) => crypto.createHmac(algorithm, key).update(base_string);
+  let hash_function = (base_string, key = tokenId, algo) => crypto.createHmac(algo, key).update(base_string);
 
-
-    const that = this;
-    Object.defineProperties(that ,{
-        'oauth' :{
-            enumerable : false,
-            configurable: false,
-        get: ()=> OAuth({
-        consumer: {
+  const that = this;
+  Object.defineProperties(that, {
+    'oauth': {
+      enumerable: false,
+      configurable: false,
+      get: () =>
+        OAuth({
+          consumer: {
             key: consumerKey,
             secret: consumerSecret
-        },
-        realm: account,
-        signature_method,
-        hash_function : hash_function.digest('base64')
-    })
-        },
-
-    'hash_function' :{
-            configurable: false,
-            get: ()=> hash_function,
-        set:(newHashFn)=>{
-                if(typeof newHashFn !== 'function') throw new TypeError('must be a function');
-                hash_function = newHashFn;
-        }
+          },
+          realm: account,
+          signature_method,
+          hash_function: (base, key) => hash_function(base, key, algoToSigMethodMap[signature_method]).digest('base64')
+        })
     },
-        'headers' : {
-            get : ()=>Object.assign({},{
-                  'Content-Type': 'application/json',
-                  ...[this.oauth].reduce(oauth=>oauth.toHeader(oauth.authorize(that.request_data, that.token)),{});
-            })
-        }
-    });
+
+    'hash_function': {
+      configurable: false,
+      get: () => hash_function,
+      set: (newHashFn) => {
+        if (typeof newHashFn !== 'function') throw new TypeError('must be a function');
+        hash_function = newHashFn;
+      }
+    },
+    'headers': {
+      get: () => ({
+        'Content-Type': 'application/json',
+        ...[that.oauth].reduce((out, oauth) => oauth.toHeader(oauth.authorize(that.request_data, that.token)), {})
+      })
+    }
+  });
 }
 
 /**
@@ -76,24 +75,28 @@ function NetSuiteOAuth(url, method, consumerKey, consumerSecret, tokenId, tokenS
  * @throws Error when method does not match constructor
  */
 NetSuiteOAuth.prototype.get = function () {
-    if( this.request_data.method !== 'get' ) throw new Error('your connection is for a `get` request');
-    return new Promise((resolve, reject) => {
-        request({
-            url: this.request_data.url,
-            method: this.request_data.method,
-            headers: this.headers
-        }, function (error, response, body) { //
-            if (error || !/^2/.test(response.statusCode)){ //
-                console.log('Body data:', body);
-                reject(body || error);
-            }
-            else {
-                if (typeof body == 'string') body = JSON.parse(body);
-                resolve(body || error);
-            }
-        });
-
-    });
+  if (this.request_data.method.toLowerCase() !== 'get') throw new Error(`your connection is for a '${this.request_data.method}' request`);
+  return new Promise((resolve, reject) => {
+    request(
+      {
+        url: this.request_data.url,
+        method: this.request_data.method,
+        headers: this.headers
+      },
+      (error, response, body) => {
+        //
+        if (error || !/^2/.test(response.statusCode)) {
+          //
+          console.log('Body data:', body);
+          reject(body || error);
+        } else {
+          // eslint-disable-next-line no-param-reassign
+          if (typeof body === 'string') body = JSON.parse(body);
+          resolve(body || error);
+        }
+      }
+    );
+  });
 };
 
 /**
@@ -102,32 +105,36 @@ NetSuiteOAuth.prototype.get = function () {
  * @throws Error when method does not match constructor
  */
 NetSuiteOAuth.prototype.post = function (data) {
-    if( this.request_data.method !== 'post' ) throw new Error('your connection is for a `post` request');
-    return new Promise((resolve, reject) => {
-        request({
-            url: this.request_data.url,
-            method: this.request_data.method,
-            json: data,
-            headers: this.headers
-        }, function (error, response, body) {
-            if (error || !/^2/.test(response.statusCode)){ //
-                console.log('Body data:', body);
-                reject(body || error);
+  if (this.request_data.method.toLowerCase() !== 'post') throw new Error(`your connection is for a '${this.request_data.method}' request`);
+  return new Promise((resolve, reject) => {
+    request(
+      {
+        url: this.request_data.url,
+        method: this.request_data.method,
+        json: data,
+        headers: this.headers
+      },
+      (error, response, body) => {
+        if (error || !/^2/.test(response.statusCode)) {
+          //
+          console.log('Body data:', body);
+          reject(body || error);
+        } else {
+          if (typeof body === 'string') {
+            try {
+              // eslint-disable-next-line no-param-reassign
+              body = JSON.parse(body);
+              // eslint-disable-next-line no-shadow
+            } catch (error) {
+              console.log('unable to parse response body');
+              reject(error);
             }
-            else {
-                if (typeof body == 'string') {
-                    try {
-                        body = JSON.parse(body);
-                    } catch (error) {
-                        console.log('unable to parse response body');
-                        reject(error);
-                    }
-                }
-                resolve(body || error);
-            }
-        });
-
-    });
+          }
+          resolve(body || error);
+        }
+      }
+    );
+  });
 };
 
 /**
@@ -136,30 +143,36 @@ NetSuiteOAuth.prototype.post = function (data) {
  * @throws Error when method does not match constructor
  */
 NetSuiteOAuth.prototype.put = function (data) {
-    if( this.request_data.method !== 'put' ) throw new Error('your connection is for a `put` request');
-    return new Promise((resolve, reject) => {
-        request({
-            url: this.request_data.url,
-            method: this.request_data.method,
-            json: data,
-            headers: this.headers
-        }, function (error, response, body) {
-            if (error || !/^2/.test(response.statusCode)){ //
-                console.log('Body data:', body);
-                reject(body || error);
+  if (this.request_data.method.toLowerCase() !== 'put') throw new Error(`your connection is for a '${this.request_data.method}' request`);
+  return new Promise((resolve, reject) => {
+    request(
+      {
+        url: this.request_data.url,
+        method: this.request_data.method,
+        json: data,
+        headers: this.headers
+      },
+      (error, response, body) => {
+        if (error || !/^2/.test(response.statusCode)) {
+          //
+          console.log('Body data:', body);
+          reject(body || error);
+        } else {
+          if (typeof body === 'string') {
+            try {
+              // eslint-disable-next-line no-param-reassign
+              body = JSON.parse(body);
+              // eslint-disable-next-line no-shadow
+            } catch (error) {
+              console.log('unable to parse response body');
+              reject(error);
             }
-            else {
-                if (typeof body == 'string') {
-                    try {
-                        body = JSON.parse(body);
-                    } catch (error) {
-                        console.log('unable to parse response body');
-                        reject(error);
-                    }
-                }
-                resolve(body || error);
-            }
-        });
-
-    });
+          }
+          resolve(body || error);
+        }
+      }
+    );
+  });
 };
+
+module.exports = NetSuiteOAuth;
